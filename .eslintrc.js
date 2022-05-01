360bd8c71a9093ecc96252e0356a87348b2f7f61
Nientai Ho <hntddt123@gmail.com>
2022-04-30 20:08:08 -0500

    Added eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'warn',
+    'no-underscore-dangle': 'warn',
+    'comma-dangle': 'off',
+    'linebreak-style': 'warn',
+    'object-curly-newline': 'off',
+    'default-case': 'off',
+    'object-shorthand': 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-return-assign': 'warn',
+    'no-unused-vars': 'warn',
+    'no-param-reassign': 'warn',
+    'max-len': ['warn', { code: 150 }],
+    'jsx-quotes': ['error', 'prefer-single']
+  }
+};
