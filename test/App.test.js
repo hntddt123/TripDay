f76c1f519e80cb5a16984c36ac9b482e93365318
Nientai Ho <hntddt123@gmail.com>
2022-04-30 20:09:01 -0500

    Added canary test

--- test/App.test.js
+++ test/App.test.js
@@ -1,5 +1,10 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../src/App';
+
+test('Canary Test', () => {
+  expect(1).toBe(1);
+});
 
 test('renders learn react link', () => {
   render(<App />);
