1608dfc5069ea561a51a834562114a418c5daad2
Nientai Ho <hntddt123@gmail.com>
2022-05-01 09:26:50 -0500

    Updated jest config moduleMapper path

--- jest.config.js
+++ jest.config.js
@@ -90,7 +90,7 @@ module.exports = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$"
-    : "<rootDir>/__mock_/imageMock.js",
+    : "<rootDir>/__mock__/imageMock.js",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy"
   },
 
