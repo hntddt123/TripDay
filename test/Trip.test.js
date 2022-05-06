c9d2e639e6466c2c8f9d59ddf87b108df2ca9e9b
Nientai Ho <hntddt123@gmail.com>
2022-05-05 21:39:15 -0500

    Added TripBoard test

--- test/Trip.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Trip from '../src/components/Trip';
-
-test('renders NewAdventureButton button', () => {
-  render(<Trip />);
-  const ButtonElement = screen.getByText(/Trips UI Here/i);
-  expect(ButtonElement).toBeInTheDocument();
-});
