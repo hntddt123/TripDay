3ab6d684f0b9f0cb98c390c1a740034354bdbfaf
Nientai Ho <hntddt123@gmail.com>
2022-05-05 21:39:04 -0500

    Added TripBoard and TripCard

--- src/components/Trip.jsx
+++
@@ -1,9 +0,0 @@
-function Trip() {
-  return (
-    <div>
-      Trips UI Here
-    </div>
-  );
-}
-
-export default Trip;
---
+++ src/components/TripCard.jsx
@@ -0,0 +1,12 @@
+function TripCard() {
+  return (
+    <div className='card border-amber-400 ml-40'>
+      <h1 className='text-4xl mb-4'>Activity</h1>
+      <button type='button' className='button text-xl'>
+        Select Activity
+      </button>
+    </div>
+  );
+}
+
+export default TripCard;
