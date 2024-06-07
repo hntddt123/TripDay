abe8286b453c46c0587401d4c318a6e32af623d5
Nientai Ho <hntddt123@gmail.com>
2024-06-07 08:52:16 +0800

    Added VITE_API_KEY using .env files

--- TripDayFrontend/src/api/dogsSliceAPI.ts
+++ TripDayFrontend/src/api/dogsSliceAPI.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import fetch from 'isomorphic-fetch';
 
-const API_KEY = 'live_KClLuXDbZ6YBUVLl9QyMqqpOJmDJKrgNOg7lKTHOti3KBuuN8LX4EAbTyuaHbRYc';
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 interface Breed {
   id: string;
