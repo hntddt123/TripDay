1e4ebaaf71a8ed79aca52a4a206597feeed98ad8
Nientai Ho <hntddt123@gmail.com>
2024-06-09 14:43:07 +0800

    Added https for dev mode

--- TripDayFrontend/vite.config.js
+++ TripDayFrontend/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
   base: '/',
@@ -10,6 +12,10 @@ export default defineConfig({
     host: true,
   },
   server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'ssl/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'ssl/cert.pem')),
+    },
     port: 5173,
     strictPort: true,
     host: true,
