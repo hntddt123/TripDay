b03054647a1440ec14d22c9c8123d35f681dd3bb
Nientai Ho <hntddt123@gmail.com>
2024-06-19 18:16:03 +0800

    Updated custom ssl keyname

--- TripDayFrontend/vite.config.js
+++ TripDayFrontend/vite.config.js
@@ -13,8 +13,8 @@ export default defineConfig({
   },
   server: {
     https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'ssl/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'ssl/cert.pem')),
+      key: fs.readFileSync(path.resolve(__dirname, 'ssl/tripdaykey.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'ssl/tripdaycert.pem')),
     },
     port: 5173,
     strictPort: true,
