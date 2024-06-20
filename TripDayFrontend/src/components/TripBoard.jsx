899c1ac3114400cb73c18147e98f9b4be5268cb8
Nientai Ho <hntddt123@gmail.com>
2024-06-20 11:21:34 +0800

    Updated styles for title and cardTrip

--- TripDayFrontend/src/components/TripBoard.jsx
+++ TripDayFrontend/src/components/TripBoard.jsx
@@ -2,9 +2,8 @@ import PropTypes from 'prop-types';
 import CustomButton from './CustomButton';
 
 function TripBoard({ label, component }) {
-
   return (
-    <div className='container card text-left mx-auto border-lime-200'>
+    <div className='cardTrip text-left mx-auto'>
       <div className='grid'>
         <h1 className='cardTitle text-4xl mb-4'>{label}</h1>
         <div>
